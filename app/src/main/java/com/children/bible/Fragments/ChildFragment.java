ec40c968907862d83825f9b42cac0f632567a7ed
Chaitanya243 <chaitanya.aggarwal243@h3u.com>
2018-06-18 18:16:17 +0530

    update

--- app/src/main/java/com/children/bible/Fragments/ChildFragment.java
+++ app/src/main/java/com/children/bible/Fragments/ChildFragment.java
@@ -8,16 +8,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.children.bible.DetailActivity;
 import com.children.bible.R;
 
+import javax.security.auth.callback.Callback;
+
 public class ChildFragment extends Fragment {
 
     private Context mContext;
-private WebView webview_child_story;
+    private WebView webview_child_story;
 
 
     @Override
@@ -28,26 +31,31 @@ private WebView webview_child_story;
         DetailActivity activity = (DetailActivity) getActivity();
         int position = activity.getPosition();
 
-        String file_name =   this.mContext.getResources().getStringArray(R.array.bibleIndex)[position];
-        file_name=file_name.replaceAll(" ", "_");
-        String url ="https://bibleforchildren.org/PDFs/english/"+file_name+"_English.pdf";
-        System.out.println("HRA REPORT URL : "+url);
-        webview_child_story=rootView.findViewById(R.id.webview_child_story);
+        String file_name = this.mContext.getResources().getStringArray(R.array.bibleIndex)[position];
+        file_name = file_name.replaceAll(" ", "_");
+        String url = "https://bibleforchildren.org/PDFs/english/" + file_name + "_English.pdf";
+        System.out.println("HRA REPORT URL : " + url);
+        webview_child_story = rootView.findViewById(R.id.webview_child_story);
+
         webview_child_story.getSettings().setJavaScriptEnabled(true);
-        webview_child_story.setWebViewClient(new WebViewClient() {
+        webview_child_story.getSettings().setPluginState(WebSettings.PluginState.ON);
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + url);
-                return true;
-            }
-        });
-
+        //---you need this to prevent the webview from
+        // launching another browser when a url
+        // redirection occurs---
+        webview_child_story.setWebViewClient(new Callback());
+        webview_child_story.loadUrl("http://docs.google.com/gview?embedded=true&url=" + url);
 
 
         return rootView;
     }
 
-
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(
+                WebView view, String url) {
+            return (false);
+        }
+    }
 }
 
